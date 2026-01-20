48108e3882f1371a7132b43e7e0444f822aa6c7c
raghwesh pathak <raghweshpathak@gmail.com>
2026-01-21 02:50:14 +0530

    feat: client dashboard with secure auth flow

--- auth/logout.php
+++ auth/logout.php
@@ -1,5 +1,34 @@
 <?php
 session_start();
+
+/*
+ * Unset all session variables
+ */
+$_SESSION = [];
+
+/*
+ * Delete session cookie (important)
+ */
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+/*
+ * Destroy the session
+ */
 session_destroy();
-header('Location: login.php');
+
+/*
+ * Redirect to login page
+ */
+header("Location: login.php");
 exit;
